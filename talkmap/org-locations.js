8bd23d967c86988f58bc01d69678f965cf7d032b
dbevivino <104077791+dbevivino@users.noreply.github.com>
2025-09-16 20:58:01 +0000

    pic

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,1 +1,77 @@
-var addressPoints = [];
+var addressPoints = [
+  [
+    "Eye-tracking Workshop<br />Rencontres Jeunes Chercheurs en Parole (RJCP); Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "Data from online production experiments and challenges for collecting good-quality recordings for prosodic analyses<br />the International Congress of Phonetic Sciences (ICPhS 20); Prague, Czechia",
+    50.0874654,
+    14.4212535
+  ],
+  [
+    "Priming boundaries in production: Data from French<br />Speech Prosody 2024 (SP2024); Leiden, The Netherlands",
+    52.1594747,
+    4.4908843
+  ],
+  [
+    "Breaking the French Accentual Phrase: Does priming prosodic phrasing modulate sentence processing?<br />the LSA Summer Institute at Umass; Amherst (MA), USA",
+    37.4625745,
+    -79.1184939
+  ],
+  [
+    "Attention and labelling in monolingual and bilingual caregiver-infant interactions<br />the International Congress of Infant Studies (ICIS); Ottawa, Canada",
+    45.4208777,
+    -75.6901106
+  ],
+  [
+    "Priming prosodic structure in production in English: A pilot study<br />the Laboratory of Excellence \u201cEmpirical Foundations of Linguistics\u201d (LabEx EFL) Annual Symposium; Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "Talk 1 on Relevant Topic in Your Field<br />UC San Francisco, Department of Testing; San Francisco, CA, USA",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Does priming prosodic phrasing modulate sentence processing? An eye-tracking study<br />MEDAL Summer School in Experimental Linguistics at the Max Planck Institute for Psycholinguistics; Nijmegen, The Netherlands",
+    51.8425749,
+    5.8389606
+  ],
+  [
+    "Caregivers adjust their speech in response to bilingual infants\u2019 attentional preferences<br />ABC'21: Language and cognition in typical and atypical development at the Chinese University of Hong Kong/virtual; Hong Kong, China",
+    22.350627,
+    114.1849161
+  ],
+  [
+    "Priming prosodic boundaries across constructions<br />Linguistic Evidence 2022; Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "A cross-linguistic and acquisitional perspective on prosodic priming<br />the PhD's seminar SFL-LLF; Paris, France",
+    48.8534951,
+    2.3483915
+  ],
+  [
+    "Does priming prosodic phrasing modulate reading times? Evidence from audio-primed acceptability judgment experiments<br />Architectures and Mechanisms for Language Processing (AMLaP 29); Donostia-San Sebasti\u00e1n, Basque Country, Spain",
+    43.3224219,
+    -1.9838889
+  ],
+  [
+    "Priming prosodic boundaries in an online speech production experiment<br />Architectures and Mechanisms for Language Processing (AMLaP 28); York, UK",
+    53.9656578,
+    -1.0743052
+  ],
+  [
+    "Priming cooperating prosodic phrasing increases reading times: An eye-tracking study<br />Architectures and Mechanisms for Language Processing (AMLaP 31); Prague, Czechia",
+    50.0874654,
+    14.4212535
+  ],
+  [
+    "Does priming prosodic phrasing modulate sentence processing? An eye-tracking study design<br />the Experimental Linguistics Workshop; Paris, France",
+    48.8534951,
+    2.3483915
+  ]
+];
